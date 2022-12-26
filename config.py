c6ad46e16ec43ef582e5f4969a0f7857141ca2ea
Matt Garcia <strongpawcoding@gmail.com>
2022-12-26 09:02:03 -0600

    Create config.py
    example config file.

---
+++ config.py
@@ -0,0 +1,4 @@
+'''This is the config file that will store the ssid and wifi passwords'''
+
+ssdi = '[your wifi network name]' #replace brackets with wifi name
+password = '[your password]' #replace brackets with wifi password
